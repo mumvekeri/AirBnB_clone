29cac023ad450f4bc30dfc534eb701c839d481ca
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-07 23:53:30 -0800

    try

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 import cmd
 import re
 import json
+import sys
+import models
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -16,5 +18,170 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """A command interpreter for the HBNB project"""
 
-    prompt = "(hbnb) " # a custom prompt
+    prompt = "(hbnb) "
     classes = ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        print()
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+    def help_quit(self):
+        """Show the help message for quit command"""
+        print("Quit command to exit the program")
+
+    def help_EOF(self):
+        """Show the help message for EOF command"""
+        print("EOF command to exit the program")
+
+    def do_create(self, arg):
+        """Create command to create a new instance of a class"""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            cls = eval(arg)
+            obj = cls()
+            obj.save()
+            print(obj.id)
+    def help_create(self):
+        """Show the help message for create command"""
+        print("Create command to create a new instance of a class")
+        print("Usage: create <class name>")
+
+    def do_show(self, arg):
+        """Show command to print the string representation of an instance"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key not in objects:
+                print("** no instance found **")
+            else:
+                print(objects[key])
+
+    def help_show(self):
+        """Show the help message for show command"""
+        print("Show command to print the string representation of an instance")
+        print("Usage: show <class name> <id>")
+
+    def do_destroy(self, arg):
+        """Destroy command to delete an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key not in objects:
+                print("** no instance found **")
+            else:
+                del objects[key]
+                storage.save()
+
+    def help_destroy(self):
+        """Show the help message for destroy command"""
+        print("Destroy command to delete an instance based on the class name and id")
+        print("Usage: destroy <class name> <id>")
+
+    def do_all(self, arg):
+        """All command to print all string representation of all instances"""
+        if not arg:
+            objects = storage.all()
+            result = []
+            for obj in objects.values():
+                result.append(str(obj))
+            print(result)
+        elif arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        else:
+            objects = storage.all(arg)
+            result = []
+            for obj in objects.values():
+                result.append(str(obj))
+            print(result)
+
+    def help_all(self):
+        """Show the help message for all command"""
+        print("All command to print all string representation of all instances")
+        print("Usage: all <class name> (optional)")
+
+    def do_update(self, arg):
+        """Update command to update an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            key = args[0] + "." + args[1]
+            objects = storage.all()
+            if key not in objects:
+                print("** no instance found **")
+            else:
+                obj = objects[key]
+                attr = args[2]
+                value = args[3]
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1]
+                try:
+                    value = eval(value)
+                except:
+                    pass
+                if attr not in ["id", "created_at", "updated_at"]:
+                    setattr(obj, attr, value)
+                obj.save()
+
+    def help_update(self):
+        """Show the help message for update command"""
+        print("Update command to update an instance based on the class name and id")
+        print("Usage: update <class name> <id> <attribute name> \"<attribute value>\"")
+
+    def do_BaseModel(self, arg):
+        """BaseModel command to retrieve all instances of BaseModel or
+        the number of instances or an instance based on its ID"""
+        if arg == "all":
+            objects = storage.all("BaseModel")
+            for obj in objects.values():
+                print(obj)
+        elif arg == "count":
+            objects = storage.all("BaseModel")
+            print(len(objects))
+        elif arg.startswith("show"):
+            id = arg[5:-1]
+            obj = storage.show("BaseModel", id)
+            if obj is None:
+                print("** no instance found **")
+            else:
+                print(obj)
+        else:
+            print("Invalid argument")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,20 +1,106 @@
 #!/usr/bin/python3
 """This is a test file for console.py"""
 
+import sys
+import os
 import unittest
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-class TestConsole(unittest.TestCase):
-    """A class to test the console"""
+class HBNBCommandTest(unittest.TestCase):
 
-    def test_create(self):
-        """Test the create command"""
-        with patch('sys.stdout', new=StringIO()) as output:
-            HBNBCommand().onecmd("create BaseModel")
-            self.assertTrue(len(output.getvalue()) > 0)
-            HBNBCommand().onecmd("create")
-            self.assertEqual(output.getvalue(), "** class name missing **\n")
-            HBNBCommand().onecmd("create MyModel")
-            self.assertEqual(output.getvalue(), "** class doesn't exist **\n")
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_quit(self, mock_stdout):
+        """Test do_quit exits the console."""
+        command = HBNBCommand()
+        self.assertTrue(command.onecmd("quit"))
+        self.assertEqual(mock_stdout.getvalue(), "")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_EOF(self, mock_stdout):
+        """Test do_EOF exits the console."""
+        command = HBNBCommand()
+        self.assertTrue(command.onecmd("EOF"))
+        self.assertEqual(mock_stdout.getvalue(), "")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_help(self, mock_stdout):
+        """Test do_help prints the help message."""
+        command = HBNBCommand()
+        command.onecmd("help")
+        self.assertEqual(mock_stdout.getvalue().startswith("Documented commands"), True)
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_create_user(self, mock_stdout):
+        """Test do_create successfully creates a User instance."""
+        command = HBNBCommand()
+        command.onecmd("create User")
+        self.assertTrue(mock_stdout.getvalue().startswith("**"))
+        self.assertEqual(len(storage.all().values()), 1)
+        instance = list(storage.all().values())[0]
+        self.assertIsInstance(instance, User)
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_create_invalid_class(self, mock_stdout):
+        """Test do_create prints error message for invalid class."""
+        command = HBNBCommand()
+        command.onecmd("create InvalidClass")
+        self.assertEqual(mock_stdout.getvalue(), "** class doesn't exist **\n")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_all(self, mock_stdout):
+        """Test do_all prints all instances of a class."""
+        user1 = User()
+        user2 = User()
+        storage.save()
+        command = HBNBCommand()
+        command.onecmd("all User")
+        output = mock_stdout.getvalue()
+        self.assertIn(str(user1), output)
+        self.assertIn(str(user2), output)
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_all_invalid_class(self, mock_stdout):
+        """Test do_all prints error message for invalid class."""
+        command = HBNBCommand()
+        command.onecmd("all InvalidClass")
+        self.assertEqual(mock_stdout.getvalue(), "** class doesn't exist **\n")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_count(self, mock_stdout):
+        """Test do_count prints the number of instances of a class."""
+        user1 = User()
+        user2 = User()
+        storage.save()
+        command = HBNBCommand()
+        command.onecmd("count User")
+        self.assertEqual(mock_stdout.getvalue(), f"There are 2 instances of User\n")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_count_invalid_class(self, mock_stdout):
+        """Test do_count prints error message for invalid class."""
+        command = HBNBCommand()
+        command.onecmd("count InvalidClass")
+        self.assertEqual(mock_stdout.getvalue(), "** class doesn't exist **\n")
+
+    @mock.patch('sys.stdout', new=StringIO())
+    def test_do_show(self, mock_stdout):
+        """Test do_show prints the string representation of an instance."""
+        user = User()
+        storage.save()
+        command = HBNBCommand()
+        command.onecmd(f"show User {user.id}")
+        self.assertIn(str(user), mock_stdout.getvalue())
+
+    @mock.patch('sys.stdout', new=StringIO())
+
