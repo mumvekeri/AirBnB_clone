224b109bd58ef3ef86d89c6187e1a563cb94fcb2
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 12:37:00 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,31 @@
 #!/usr/bin/python3
+"""trying out"""
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
-import models
-
-
 class BaseModel:
-    """A base class for other classes"""
+    """
+    Base class for all other models.
+    """
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a new instance"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "_class":
-                    continue
-                elif key in ("created_at", "updated_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return a string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[<{self.__class__.__name__}>] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the updated_at attribute and save object"""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Return a dictionary representation of the instance"""
-        result = self.__dict__.copy()
-        result["_class"] = self.__class__.__name__
-        result["created_at"] = self.created_at.isoformat()
-        result["updated_at"] = self.updated_at.isoformat()
-        return result
+        dict_repr = self.__dict__.copy()
+        dict_repr.update({
+            "__class__": self.__class__.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        })
+        return dict_repr
 
