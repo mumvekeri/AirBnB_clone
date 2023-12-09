6bebadef31385f15866cc1966373c97889902e16
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 14:50:57 -0800

    try

--- console.py
+++ console.py
@@ -13,6 +13,7 @@ from models.place import Place
 from models.review import Review
 from models import storage
 
+
 class HBNBCommand(cmd.Cmd):
     """A command interpreter for the HBNB project"""
 
@@ -34,12 +35,16 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, arg):
         """Create command to create a new instance of a class"""
-        if not arg:
+        args = shlex.split(arg)
+
+        if not args:
             print("** class name missing **")
-        elif arg not in HBNBCommand.classes:
+        elif len(args) > 1:
+            print("** too many arguments **")
+        elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
         else:
-            cls_name = arg  # Get the class name directly
+            cls_name = args[0]
             cls = globals()[cls_name]
             obj = cls()
             obj.save()
@@ -48,15 +53,15 @@ class HBNBCommand(cmd.Cmd):
     def do_show(self, arg):
         """Show command to print the string representation of an instance"""
         args = shlex.split(arg)
+
         if not args:
             print("** class name missing **")
+        elif len(args) != 2:
+            print("** invalid arguments **")
         elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
         else:
-            class_name = args[0]
-            instance_id = args[1]
+            class_name, instance_id = args
             key = class_name + "." + instance_id
             objects = storage.all()
             if key not in objects:
@@ -67,15 +72,15 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Destroy command to delete an instance based on the class name and id"""
         args = shlex.split(arg)
+
         if not args:
             print("** class name missing **")
+        elif len(args) != 2:
+            print("** invalid arguments **")
         elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
         else:
-            class_name = args[0]
-            instance_id = args[1]
+            class_name, instance_id = args
             key = class_name + "." + instance_id
             objects = storage.all()
             if key not in objects:
@@ -87,78 +92,42 @@ class HBNBCommand(cmd.Cmd):
     def do_update(self, arg):
         """Update command to update an instance based on the class name and id"""
         args = shlex.split(arg)
+
         if not args:
             print("** class name missing **")
+        elif len(args) < 4 or len(args) > 5:
+            print("** invalid arguments **")
         elif args[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-        elif len(args) == 1:
-            print("** instance id missing **")
-        elif len(args) == 2:
-            print("** dictionary missing **")
         else:
-            class_name = args[0]
-            instance_id = args[1]
+            class_name, instance_id, attribute_name, value = args[:4]
             key = class_name + "." + instance_id
             objects = storage.all()
+
             if key not in objects:
                 print("** no instance found **")
             else:
                 obj = objects[key]
                 try:
-                    attr_dict = json.loads(' '.join(args[2:]))
-                except:
-                    print("** invalid dictionary format **")
+                    # Check for valid attribute name
+                    if not hasattr(obj, attribute_name):
+                        print("** no attribute found **")
+                        return
+
+                    # Cast value to attribute type
+                    value = type(getattr(obj, attribute_name))(value)
+                    setattr(obj, attribute_name, value)
+                    obj.save()
+                except ValueError:
+                    print("** invalid value **")
                     return
-
-                if not isinstance(attr_dict, dict):
-                    print("** invalid dictionary format **")
-                    return
-
-                for key, value in attr_dict.items():
-                    setattr(obj, key, value)
-
-                obj.save()
 
     def do_all(self, arg):
         """All command to print all string representation of all instances"""
-        if not arg:
-            objects = storage.all()
-            result = []
-            for obj in objects.values():
-                result.append(str(obj))
-            print(result)
-        elif arg not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-        else:
-            class_name = arg
-            objects = storage.all(class_name)
-            result = []
-            for obj in objects.values():
-                result.append(str(obj))
-            print(result)
+        args = shlex.split(arg)
 
-    def default(self, line):
-        """Called on an input line when the command prefix is not recognized."""
-        args = shlex.split(line)
-        if len(args) >= 2 and args[0] in HBNBCommand.classes:
-            command = args[1] if args[1] in ["all", "show", "destroy", "update"] else ""
-            if command:
-                getattr(self, "do_" + command)(args[0] + " " + ' '.join(args[2:]))
-            else:
-                print("** command not recognized **")
-        else:
-            print("** command not recognized **")
-
-    def do_count(self, arg):
-        """Count command to print the number of instances of a class"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-        else:
-            class_name = arg
-            count = storage.count(class_name)
-            print("There are {} instances of {}".format(count, class_name))
+        if not args:
+            pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
