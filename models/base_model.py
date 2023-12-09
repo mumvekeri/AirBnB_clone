4976749d454ef54c7807655c6919666636fa565f
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 11:46:35 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -3,27 +3,29 @@
 
 from uuid import uuid4
 from datetime import datetime, timedelta
-
 import models
-
 
 class BaseModel:
     """A base class for other classes"""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new instance"""
-        if kwargs is not None:
+        if kwargs:
+            # Use kwargs to set attributes if provided
             for key, value in kwargs.items():
                 if key == "_class":
                     continue
-                elif key == "created_at" or key == "updated_at":
+                elif key in ("created_at", "updated_at"):
                     # Handle datetime format (assuming ISO format)
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.id = str(uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = self.created_at
-                    storage.new(self)
+                    setattr(self, key, value)
+        else:
+            # Default behavior for new instances
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the instance"""
