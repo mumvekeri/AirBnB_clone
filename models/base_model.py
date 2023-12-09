1952028aaafa83e75a93a4d62887edbf2b18ec1e
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 12:45:24 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python3
-"""Trying out"""
+"""
+Base class for all other models. This class provides common attributes and methods,
+including initialization from both keyword arguments and dictionary representations.
 
-import uuid
-from datetime import datetime
+Attributes:
+    id (str): Unique identifier for the model.
+    created_at (datetime): Date and time of the model creation.
+    updated_at (datetime): Date and time of the last model update.
+
+Methods:
+    __init__(self, *args, **kwargs): Initializes the model with arguments.
+    __str__(self): Returns a string representation of the model.
+    save(self): Updates the model's `updated_at` attribute.
+    to_dict(self): Creates a dictionary representation of the model.
+"""
 
 
 class BaseModel:
-    """
-    Base class for all other models.
-    """
 
     def __init__(self, *args, **kwargs):
+        self._initialize_from_kwargs(kwargs)
+        if not kwargs:
+            self._initialize_new_instance()
+
+    def _initialize_from_kwargs(self, kwargs):
         if kwargs:
             self.id = kwargs.pop("id", str(uuid.uuid4()))
-            self.created_at = datetime.fromisoformat(kwargs.pop("created_at"))
-            self.updated_at = datetime.fromisoformat(kwargs.pop("updated_at"))
+            try:
+                self.created_at = datetime.fromisoformat(kwargs.pop("created_at"))
+                self.updated_at = datetime.fromisoformat(kwargs.pop("updated_at"))
+            except ValueError:
+                raise ValueError("Invalid date format in kwargs")
             for key, value in kwargs.items():
                 setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+
+    def _initialize_new_instance(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[<{self.__class__.__name__}>] ({self.id}) {self.__dict__}"
