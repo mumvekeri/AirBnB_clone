bd9b921b7de80aebc0116c975412d46a59281aa1
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 13:16:08 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from file_storage import storage
 
 
 class BaseModel:
@@ -21,12 +22,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)  # Add new instance to storage
 
     def __str__(self):
         return f"[<{self.__class__.__name__}>] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()  # Save objects to JSON file
 
     def to_dict(self):
         dict_repr = self.__dict__.copy()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,71 +2,34 @@
 """This is a module that creates a file storage class"""
 
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+import re
 
 class FileStorage:
-    """A class that serializes and deserializes instances to and from a JSON file"""
+    """
+    Class for storing and retrieving objects from a JSON file.
+    """
 
-    __file_path = "file.json"  # the path to the JSON file
-    __objects = {}  # a dictionary to store all objects by <class name>.id
-    classes = {"BaseModel": BaseModel,
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review}
+    __file_path = "file.json"
+    __objects = {}
 
-    def all(self, cls=None):
-        """Returns the dictionary __objects"""
-        if cls is None:
-            return FileStorage.__objects
-        elif isinstance(cls, str):
-            if cls in FileStorage.classes:
-                cls = FileStorage.classes[cls]  # get the class object
-            else:
-                return {}
-            return {key: obj for key, obj in FileStorage.__objects.items() if isinstance(obj, cls)}
+    def __init__(self):
+        self.reload()  # Load objects from JSON file on initialization
+
+    def all(self):
+        return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id as it should be"""
-        if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
-            FileStorage.__objects[key] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        result = {}
-        for key, obj in FileStorage.__objects.items():
-            result[key] = obj.to_dict()
-
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(result, f)
+        with open(self.__file_path, "w") as file:
+            json.dump(self.__objects, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists)"""
         try:
-            with open(FileStorage.__file_path, "r") as f:
-                result = json.load(f)
-                for key, value in result.items():
-                    if value["_class"] in FileStorage.classes:
-                        cls = FileStorage.classes[value["_class_"]]
-                        obj = cls(**value)
-                        FileStorage.__objects[key] = obj
+            with open(self.__file_path, "r") as file:
+                self.__objects = json.load(file)
         except FileNotFoundError:
-            pass
-
-    def count(self, cls=None):
-        """Count the number of objects of a specific class"""
-        if cls is not None and cls in FileStorage.classes:
-            return sum(1 for obj in FileStorage.__objects.values() if isinstance(obj, FileStorage.classes[cls]))
-        else:
-            return len(FileStorage.__objects)
+            pass  # No need to raise an error if the file doesn't exist
 
