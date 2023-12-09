f37a3efe43f0fcf7da6f2f158da3d4cd9b93b4e3
mumvekeri <mumvekerichidochashe@gmail.com>
2023-12-09 12:15:33 -0800

    try

--- models/base_model.py
+++ models/base_model.py
@@ -2,46 +2,42 @@
 """This module creates a base model class"""
 
 from uuid import uuid4
-from datetime import datetime, timedelta
+from datetime import datetime
 import models
+import storage
 
 class BaseModel:
     """A base class for other classes"""
 
-    def __init__(self, *args, **kwargs):
+    def _init_(self, *args, **kwargs):
         """Initialize a new instance"""
-        if kwargs:
-            # Use kwargs to set attributes if provided
+        if kwargs is not None:
             for key, value in kwargs.items():
                 if key == "_class":
                     continue
-                elif key in ("created_at", "updated_at"):
-                    # Handle datetime format (assuming ISO format)
+                elif key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                else:
                     setattr(self, key, value)
-        else:
-            # Default behavior for new instances
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+                else:
+                    self.id = str(uuid4())
+                    self.created_at = datetime.now()
+                    self.updated_at = self.created_at
+                    storage.new(self)
+        self.class = self.__class_
 
-    def __str__(self):
+    def _str_(self):
         """Return a string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.class.__name, self.id, self.__dict_)
 
     def save(self):
-        """Update the updated_at attribute and save object"""
+        """Update the updated_at attribute with the current datetime and save it"""
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance"""
-        result = self.__dict__.copy()
-        result["_class"] = self.__class__.__name__
-        # Convert datetime objects to ISO format strings
+        result = self._dict_.copy()
+        result["class"] = self.__class.__name_
         result["created_at"] = self.created_at.isoformat()
         result["updated_at"] = self.updated_at.isoformat()
         return result
-
